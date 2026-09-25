2ee0a467cae813a9d5ef62701b100562dbf11d7b
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-09-25 14:28:11 +0200

    feat: add global stylesheet with footer and active tab styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { loadHomepage } from "./homepage.js";
 import { loadAboutpage } from "./aboutpage.js";
 import { loadMenupage } from "./menupage.js"
+import "./style.css";
 
 const home = document.querySelector("#home");
 const menu = document.querySelector("#menu");
@@ -15,6 +16,8 @@ const tabs = [
 
 tabs.forEach((tab) => {
   tab.button.addEventListener("click", () => {
+    tabs.forEach((t) => t.button.classList.remove("active"));
+    tab.button.classList.add("active");
     content.textContent = "";
     tab.load();
   });
